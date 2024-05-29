81637fa83f7f45cf400a5965fa5bdd27fecc4649
akhilkarenae <69794445+akhilkarenae@users.noreply.github.com>
2024-05-29 23:04:31 +0530

    Update CustomError.errorHandlers.js

--- errorHandlers/CustomError.errorHandlers.js
+++ errorHandlers/CustomError.errorHandlers.js
@@ -10,15 +10,3 @@ class CustomError extends Error{
 
 
 export default CustomError
-class CustomError extends Error{
-    constructor(message,status,errorCode){
-        super(message,status,errorCode)
-        this.name="CustomError"
-        this.status=status || 500
-        this.message=message || "some Internal error occured"
-        Error.captureStackTrace(this, this.constructor);
-    }
-}
-
-
-export default CustomError
