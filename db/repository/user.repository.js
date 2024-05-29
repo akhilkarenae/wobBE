83c7a569524f91f56401787f63a371a75ebbd5cf
akhilkarenae <69794445+akhilkarenae@users.noreply.github.com>
2024-05-29 23:03:16 +0530

    Update user.repository.js

--- db/repository/user.repository.js
+++ db/repository/user.repository.js
@@ -28,33 +28,3 @@ export const saveUser = async (email,hashedPassword) =>{
         console.log(err)
     }
 }
-import { UserModel } from "../models/user.model.js";
-
-
-export const findUserByEmail = async (email) => {
-    try{
-        const user = await UserModel.findOne({ email })
-        return user;
-    }catch(err){
-        console.log(err)
-    }
-}
-
-export const findUserById = async (userId) => {
-    try{
-        const user = await UserModel.findById({ _id: userId });
-        return user;
-    }catch(err){
-        console.log(err)
-    }
-}
-
-export const saveUser = async (email,hashedPassword) =>{
-    try{
-        const user = new UserModel({ email: email, password: hashedPassword });
-        const newUser = user.save();
-        return newUser;
-    }catch(err){
-        console.log(err)
-    }
-}
